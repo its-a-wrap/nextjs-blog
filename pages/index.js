3473ccb9ce01eb5130e6ef29cdcd0198b41a9d36
its-a-wrap <patrick_tweed@hotmail.co.uk>
2021-03-25 16:49:10 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
               <title>{siteTitle}</title>
           </Head>
           <section className={utilStyles.headingMd}>
-              <p>[Your Self Introduction]</p>
+              <p>Hi Dad</p>
           </section>
           <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
               <h2 className={utilStyles.headingLg}>Blog</h2>
